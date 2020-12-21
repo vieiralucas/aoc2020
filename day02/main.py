95c3ea63ef958c987948e391346b3b977cbbaf70
Lucas Vieira <vieiralucas4@gmail.com>
2020-12-20 23:03:21 -0300

    Day 2

---
+++ day02/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+def parse(line):
+    rules, password = line.split(":")
+    min_max, char = rules.split(" ")
+    minimum, maximum = min_max.split("-")
+
+    return int(minimum), int(maximum), char.strip(), password.strip()
+
+def is_valid1(minimum, maximum, char, password):
+    count = 0
+    for c in password:
+        if c == char:
+            count += 1
+
+        if count > maximum:
+            return False
+
+    return count >= minimum
+
+def part1():
+    with open("input.txt") as f:
+        result = 0
+        for line in f.readlines():
+            minimum, maximum, char, password = parse(line)
+            result = result + 1 if is_valid1(minimum, maximum, char, password) else result
+
+        print(result)
+
+def xor(b1, b2):
+    return b1 and not b2 or not b1 and b2
+
+def is_valid2(pos1, pos2, char, password):
+    return xor(password[pos1 - 1] == char, password[pos2 - 1] == char)
+
+def part2():
+    with open("input.txt") as f:
+        result = 0
+        for line in f.readlines():
+            pos1, pos2, char, password = parse(line)
+            result = result + 1 if is_valid2(pos1, pos2, char, password) else result
+
+        print(result)
+
+if __name__ == "__main__":
+    # part1()
+    part2()
+
